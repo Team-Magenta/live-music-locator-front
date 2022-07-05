2d7e317cc87aab40e285a4163fca2af8f8c8344c
Dennis DeVries <7dennisdv7@gmail.com>
2022-07-05 10:52:16 -0400

    adding basic content to header and footer beginning to make the home page

---
+++ music_locator/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import axios from 'axios';
+import './Home.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <h2>Test</h2>
+    )
+  }
+}
+
+export default Home;
---
+++ music_locator/src/components/OurTeam.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import axios from 'axios';
+import './OurTeam.css';
+
+class OurTeam extends React.Component {
+  render() {
+    return (
+      <h2>Test</h2>
+    )
+  }
+}
+
+export default OurTeam;
