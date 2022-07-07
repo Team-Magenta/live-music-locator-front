9e9d46d54a449249f70f2b75bc3c832baf8eeb72
Falashade Greene <falashadegreene@gmail.com>
2022-07-07 15:09:52 -0400

    added Card bootstrap component

--- music_locator/src/components/OurTeam.js
+++ music_locator/src/components/OurTeam.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import axios from 'axios';
 import './OurTeam.css';
-import Carousel from 'react-bootstrap/Carousel';
+// import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card'
 
 
 
@@ -10,70 +11,63 @@ class OurTeam extends React.Component {
     return (
       <>
         <h1>Our Team</h1>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./images/scottie.jpeg"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>Scottie Houghton</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./images/Stephen.jpeg"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Stephen Martinez</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/150"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Marvin Bonds</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/150"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>Dennis Devries</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/150"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>Falashade Greene</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+        <div class="cardgroup">
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="./images/stephen.png" class="stephen"/>
+          <Card.Body>
+            <Card.Title>Stephan Martinez</Card.Title>
+            <Card.Text >
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="./images/scottie.jpeg" />
+          <Card.Body>
+            <Card.Title>Scottie Houghton</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150"/>
+          <Card.Body>
+            <Card.Title>Marvin Bonds</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Dennis Devries</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="https://via.placeholder.com/150" />
+          <Card.Body>
+            <Card.Title>Falashade Greene</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+       </div>
       </>
     )
   }
 }
+
+
 
 export default OurTeam;
