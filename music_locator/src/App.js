ebd01ed705efa23efbea43a3680ef1ef49569f6d
Dennis DeVries <7dennisdv7@gmail.com>
2022-07-07 17:50:30 -0400

    adding login button initializeing auth0

--- music_locator/src/App.js
+++ music_locator/src/App.js
@@ -1,3 +1,7 @@
+import { withAuth0 } from "@auth0/auth0-react";
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
+import Profile from './Profile';
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,32 +19,36 @@ import {
 class App extends React.Component {
   render() {
     return (
-      <div className="body">
-        <Router>
-          <Header />
+      <>
+        {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        {this.props.auth0.isAuthenticated ? <Profile/> : <h2>Please Login</h2>}
+        <div className="body">
+          <Router>
+            <Header />
             <Routes>
               <Route
                 exact path="/"
-                element={<Home/>}
+                element={<Home />}
               >
               </Route>
               <Route
                 exact path="/MyEvents"
-                element={<Search/>}
+                element={<Search />}
               >
               </Route>
               <Route
                 exact path="/OurTeam"
-                element={<OurTeam/>}
+                element={<OurTeam />}
               >
               </Route>
             </Routes>
-            <Footer/>
-        </Router>
-      </div>
+            <Footer />
+          </Router>
+        </div>
+      </>
     )
   }
-  
+
 }
 
-export default App;
+export default withAuth0(App);
--- music_locator/src/components/Home.js
+++ music_locator/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, Card, Container } from 'react-bootstrap';
+import { Form, Button, Card } from 'react-bootstrap';
 import axios from 'axios';
 import './Home.css';
 import { format, parseISO } from 'date-fns'
