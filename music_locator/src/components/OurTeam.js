7ae9b7d4d2a731c5b1ace9a6c3634b12a9c4b67e
Falashade Greene <falashadegreene@gmail.com>
2022-07-06 23:52:20 -0400

    started building the skeleton of about team page

--- music_locator/src/components/OurTeam.js
+++ music_locator/src/components/OurTeam.js
@@ -1,11 +1,77 @@
 import React from 'react';
 // import axios from 'axios';
 import './OurTeam.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+
 
 class OurTeam extends React.Component {
   render() {
     return (
-      <h2>Test Our Team</h2>
+      <>
+        <h1>Our Team</h1>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="./images/scottie.jpeg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Scottie Houghton</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="./images/Stephen.jpeg"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Stephen Martinez</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://via.placeholder.com/150"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Marvin Bonds</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://via.placeholder.com/150"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Dennis Devries</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://via.placeholder.com/150"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>Falashade Greene</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </>
     )
   }
 }
