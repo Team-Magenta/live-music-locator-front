ca48fdc74a2ae45d3f48fabb5533cb33947975db
Marvin Bonds <marvin.bonds@gmail.com>
2022-07-07 19:34:35 -0500

    update

--- music_locator/src/App.js
+++ music_locator/src/App.js
@@ -1,10 +1,57 @@
+// import './App.css';
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+// import Home from './components/Home';
+// import Search from './components/MyEvents';
+// import OurTeam from './components/OurTeam';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom"
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <Router>
+//           <Header />
+//             <Routes>
+//               <Route
+//                 exact path="/"
+//                 element={<Home/>}
+//               >
+//               </Route>
+//               <Route
+//                 exact path="/MyEvents"
+//                 element={<Search/>}
+//               >
+//               </Route>
+//               <Route
+//                 exact path="/OurTeam"
+//                 element={<OurTeam/>}
+//               >
+//               </Route>
+//             </Routes>
+//             <Footer/>
+//         </Router>
+//       </>
+//     )
+//   }
+  
+// }
+
+// export default App;
+
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-import Search from './components/MyEvents';
+import MyEvents from './components/MyEvents';
 import OurTeam from './components/OurTeam';
 import {
   BrowserRouter as Router,
@@ -15,7 +62,7 @@ import {
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div className="body">
         <Router>
           <Header />
             <Routes>
@@ -26,7 +73,7 @@ class App extends React.Component {
               </Route>
               <Route
                 exact path="/MyEvents"
-                element={<Search/>}
+                element={<MyEvents/>}
               >
               </Route>
               <Route
@@ -37,7 +84,7 @@ class App extends React.Component {
             </Routes>
             <Footer/>
         </Router>
-      </>
+      </div>
     )
   }
   
--- music_locator/src/components/Home.js
+++ music_locator/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Form, Button, Card, Container } from 'react-bootstrap';
 import axios from 'axios';
 import './Home.css';
+import { format, parseISO } from 'date-fns'
 
 class Home extends React.Component {
   constructor(props) {
@@ -29,12 +30,33 @@ class Home extends React.Component {
     // });
   };
 
+  handleEventSubmit = (event) => {
+    let newEvent = {
+      image: event.image,
+      venue: event.venue[0].name,
+      artist: event.artist,
+      date: event.date,
+    }
+    console.log(newEvent)
+    this.postEvents(newEvent)
+  }
+  postEvents = async (newEventObj) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/events`;
+      let createEvent = await axios.post(url, newEventObj);
+      this.setState({
+        events: [...this.state.events, createEvent.data]
+      });
+    } catch (error) {
+      console.log('error: ', error.response.data)
+    }
+  }
   handleCityInput = (e) => {
     this.setState({
       city: e.target.value
     });
   };
-  
+
   // componentDidMount() {
   //   this.getEvents();
   // }
@@ -48,8 +70,8 @@ class Home extends React.Component {
   //       events: results.data
   //     })
   //   } catch (error) {
-      // console.log('error: ', error.response.data)
-    // }
+  // console.log('error: ', error.response.data)
+  // }
   // }
 
 
@@ -66,7 +88,7 @@ class Home extends React.Component {
   //       error: true,
   //       errorMessage: `An error occurred: ${error.reponse.status}`
   //     })
-      
+
   //   }
   // }
   postEvents = async (event) => {
@@ -74,61 +96,90 @@ class Home extends React.Component {
       let url = `${process.env.REACT_APP_SERVER}/events`;
       let createdEvent = await axios.post(url, event);
       this.setState({
-        events: [...this.state.events, createdEvent.data]})
-      } catch(error){
-        console.log('we have an error: ', error.response)
-      }
+        events: [...this.state.events, createdEvent.data]
+      })
+    } catch (error) {
+      console.log('we have an error: ', error.response)
+    }
   }
 
   render() {
     // function to map through the data retrieved from the server which are stored in "events" in state
     // {event.venue} and other elements will have to match items that are in the data set that is sent from the api
-    
+
     let events = this.state.events.map((event, idx) => {
-      return <Container>
-        <Card>
+
+      return (
+        // <Container className='mt-4'>
+        <Card className="card-container" key={idx} style={{ width: '18rem', margin: 25 }}>
           <Card.Img
+            className="card-image"
             variant="top"
-            // style={{cursor: 'pointer'}}
             src={event.image}
             alt={event.title}
-            title={event.title}
-          />
+            title={event.title} />
           <Card.Body>
-          <Card.Header>{event.venue[0].name}</Card.Header>
-          <Card.Title>{event.artist}</Card.Title>
-          <Card.Text>{event.date}</Card.Text>
+            <Card.Title>{event.venue[0].name}</Card.Title>
+            <Card.Text className="mb-2 pt-2 h-100">
+              {event.artist}
+            </Card.Text>
+            <Card.Text>
+              {format(parseISO(event.date), 'PPPP')}
+            </Card.Text>
+            <Button onClick={() => this.handleEventSubmit(event)}variant="light">Add to My Events</Button>
           </Card.Body>
-          <Button onClick={()=> {this.postEvents(event)}}>Add to My Events</Button>
         </Card>
-      </Container>
+        // </Container>
+      )
+      // return <div class="card-div">
+      //   <Container className='mt-4'>
+      //     <Card style={{ width: '18rem' }} className="h-100 p-3 card-container">
+      //       <Card.Img
+      //         className="card-image"
+      //         variant="top"
+      //         // style={{cursor: 'pointer'}}
+      //         src={event.image}
+      //         alt={event.title}
+      //         title={event.title}
+      //       />
+      //       <Card.Body>
+      //         <Card.Header>{event.venue[0].name}</Card.Header>
+      //         <Card.Title>{event.artist}</Card.Title>
+      //         <Card.Text>{event.date}</Card.Text>
+      //       </Card.Body>
+      //       <Button onClick={() => this.handleEventSubmit(event)}>Add to My Events</Button>
+      //     </Card>
+      //   </Container>
+      // </div>
     });
     return (
-      <>
-        <h2>Test Home</h2>
-        <p>testing testing testing</p>
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Group
-            className="mb-3"
-            controlId="location"
-          >
-            <Form.Label>By City</Form.Label>
-            <Form.Control
-              type="text"
-              onInput={this.handleCityInput}
-              placeholder="City"
-            ></Form.Control>
-          </Form.Group>
-          <Button
-            type="submit"
-            variant="primary">
-            Submit
-          </Button>
-        </Form>
-        <div>
+      <div className="d-flex h-100">
+
+
+        <div className="form-div">
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group
+              className="mb-3"
+              controlId="location"
+            >
+              <Form.Label>By City</Form.Label>
+              <Form.Control
+                type="text"
+                onInput={this.handleCityInput}
+                placeholder="City"
+              ></Form.Control>
+            </Form.Group>
+            <Button
+              type="submit"
+              variant="primary">
+              Submit
+            </Button>
+          </Form>
+        </div>
+        <div className="background">
           {events}
         </div>
-      </>
+      </div>
     )
   }
 }
